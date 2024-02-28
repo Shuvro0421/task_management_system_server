a09bbeeb674099673e7157ac1dd061216606e732
Shuvro0421 <adibarmanshuvro89@gmail.com>
2024-02-28 21:26:41 +0600

    jwt token done

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ const verifyToken = (req, res, next) => {
     next();
   });
 };
-
 const uri = `mongodb+srv://${process.env.VITE_DB_USER}:${process.env.VITE_DB_PASS}@cluster0.04lxrta.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -54,10 +53,6 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-//   const jobsCollection = client.db("jobsDB").collection("jobs");
-//   const appliedJobsCollection = client.db("jobsDB").collection("appliedJobs");
-//   const reviewsCollection = client.db("jobsDB").collection("reviews");
-
   try {
     // jwt token
     app.post("/jwt", logger, async (req, res) => {
@@ -80,7 +75,7 @@ async function run() {
       res.clearCookie("token", { maxAge: 0 }).send({ success: true });
     });
 
-
+  
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -91,3 +86,4 @@ run().catch(console.dir);
 app.listen(port, () => {
   console.log("Task management system is running on port", port);
 });
+
